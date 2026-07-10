cf88515bed4b1ecc53532c42d83fb8df908e58d3
RouaTabarkii <tbarkirouee@gmail.com>
2026-07-10 21:06:04 +0100

    llm

--- llm_lambda/main.py
+++ llm_lambda/main.py
@@ -3,26 +3,40 @@ import os
 from groq import Groq
 import pypdf 
 
+import streamlit as st
 import os
 print(os.getcwd())
 
-reader = pypdf.PdfReader("../Lettre de Motivation.pdf")
-print(len(reader.pages
-          ))
+
+
+uploaded_file = st.file_uploader("Choose a pdf file", type="pdf")
+
+if uploaded_file is not None: 
+
+    reader = pypdf.PdfReader(uploaded_file)
+
+    doc = ""
+
+    for i in range(len(reader.pages)):
+        text = reader.pages[i].extract_text()
+        doc += text
+        # print(text)
 
 client = Groq( 
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-user_prmpt = input("Enter your prompt: ")
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": user_prmpt,
-        }
-    ],
-    model="meta-llama/llama-4-scout-17b-16e-instruct",
-)
+user_prmpt = st.text_input("Enter your prompt:")
 
-print(chat_completion.choices[0].message.content)
+if user_prmpt:
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_prmpt+ "\n\n" + doc
+            }
+        ],
+        model="meta-llama/llama-4-scout-17b-16e-instruct",
+    )
+
+    st.write(chat_completion.choices[0].message.content)
